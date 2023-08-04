acabe3e6b0fc2d565134a86fe83e0a62527b970a
정민서 <102645965+MinnSeoo@users.noreply.github.com>
2023-08-04 10:41:01 +0900

    Update 짝수는 싫어요.py
    📝 num_list를 정렬하여 새로운 sort_list 생성 코드 추가

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,8 +1,10 @@
 def solution(n):
-    sort_list = []
+    num_list = []
     cnt = 1
     while n >= cnt:
-        if cnt % 2 != 0:
-            sort_list.append(cnt)
+        if cnt % 2 != 0:    # 홀수이면 list에 추가
+            num_list.append(cnt)
         cnt += 1
+
+    sort_list = sorted(num_list)    # num_list를 정렬해 새로운 sort_list에 값 저장
     return sort_list
