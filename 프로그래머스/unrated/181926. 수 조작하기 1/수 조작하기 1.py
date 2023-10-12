508d1bf01a057187adda34683c40bb1851ee4f62
정민서 <102645965+MinnSeoo@users.noreply.github.com>
2023-10-12 09:03:49 +0900

    [level unrated] Title: 수 조작하기 1, Time: 1.88 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,5 +1,5 @@
 def solution(n, control):
-    
+# case1
     for char in control:
         if char == "w":
             n += 1
@@ -10,3 +10,7 @@ def solution(n, control):
         elif char == "d":
             n += 10
     return n
+
+# case2
+    # key = dict(zip(["w","s","d","a"], [1,-1,10,-10]))
+    # return n + sum(key[c] for c in control)
