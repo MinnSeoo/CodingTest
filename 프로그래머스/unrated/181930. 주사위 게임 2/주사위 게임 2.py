6b360f26517f992bd736cd92b293bc4a1b21dc34
MinnSeoo <ms.rry01@gmail.com>
2023-10-14 19:20:03 +0900

    "case2 문제 풀이 추가!"

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,3 +1,4 @@
+# case 1
 def solution(a, b, c):
     if a == b and b == c:
         result = (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)  # 세 숫자가 모두 같은 경우
@@ -7,3 +8,14 @@ def solution(a, b, c):
         result = a + b + c   # 세 숫자가 모두 다른 경우
 
     return result
+
+
+# case 2
+def solution(a, b, c):
+    check=len(set([a,b,c]))
+    if check==1:                        # 세 숫자 모두 같을 때
+        return 3*a*3*(a**2)*3*(a**3)
+    elif check==2:                      # 두 숫자만 같을 때
+        return (a+b+c)*(a**2+b**2+c**2)
+    else:                               # 세 숫자가 모두 다를 때
+        return (a+b+c)
