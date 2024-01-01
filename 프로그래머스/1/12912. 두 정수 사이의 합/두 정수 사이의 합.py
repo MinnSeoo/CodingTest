493bf68b412837b7c2ce8eda817c6279d085f62f
정민서 <102645965+MinnSeoo@users.noreply.github.com>
2024-01-01 23:31:31 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.15 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -0,0 +1,12 @@
+def solution(a,b):
+    hap = 0
+    
+    if a <= b:
+        for i in range(a,b+1):
+            hap += i
+        return hap
+    
+    else:
+        for i in range(b,a+1):
+            hap += i
+        return hap
