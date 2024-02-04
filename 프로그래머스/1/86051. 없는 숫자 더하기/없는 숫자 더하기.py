91c4066281a8c67b7989237683f23a2a3bc05e0e
정민서 <102645965+MinnSeoo@users.noreply.github.com>
2024-02-04 22:13:15 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -2,6 +2,10 @@ def solution(numbers):
     answer = 0
     for i in range(10):
         if i not in numbers:
+            print(i)
             answer += i
-    
     return answer
+
+# Test 
+numbers = [1, 2, 3, 4, 6, 7, 8, 0]
+print(solution(numbers))
