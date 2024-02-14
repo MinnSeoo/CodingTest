509bd5f01f4bce44bbf6b0c8a0bef287dd2b0f2c
정민서 <102645965+MinnSeoo@users.noreply.github.com>
2024-02-14 23:40:07 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -0,0 +1,14 @@
+def solution(t,p):
+    new_list = []   
+    list_t = list(t)
+    result = 0
+    
+    while(len(list_t) >= len(p)):
+        tmp = ''
+        for i in range(len(p)):
+            tmp += list_t[i]
+        new_list.append(tmp)
+        list_t.pop(0)    
+    
+    a = [1 for num in new_list if num <= p ]
+    return len(a)
